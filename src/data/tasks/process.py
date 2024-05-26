8410bbb1c4fda95fe9d6010a7f461032947f7e1b
phdkhanh2507 <phdkhanh.2507@gmail.com>
2024-05-26 15:01:41 +0700

    chore: change data directory

--- src/data/tasks/process.py
+++ src/data/tasks/process.py
@@ -67,7 +67,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--cache-dir",
         type=str,
-        default=os.path.join(os.getcwd(), "data/.cache"),
+        default=os.path.join(os.getcwd(), "data/external/.cache"),
         help="Cache directory.",
     )
     return parser.parse_args()
