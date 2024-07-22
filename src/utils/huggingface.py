c367ef1276e6e82a17cec0a6e0ed0c221ae385cd
tanthinhdt <tanthinhdt24@gmail.com>
2024-07-22 23:52:02 +0700

    update(src/utils/huggingface): use pathlib

--- src/utils/huggingface.py
+++ src/utils/huggingface.py
@@ -1,7 +1,9 @@
 import os
+import logging
 import tempfile
 from glob import glob
-from logging import Logger
+from typing import Union
+from pathlib import Path
 from huggingface_hub import HfApi, HfFileSystem, CommitScheduler
 from .zipping import zip_dir
 
@@ -16,7 +18,6 @@ class UploadScheduler(CommitScheduler):
     def __init__(
         self,
         *,
-        logger: Logger,
         glob_pattern: str = None,
         delete_after_upload: bool = False,
         overwrite: bool = False,
@@ -25,34 +26,30 @@ class UploadScheduler(CommitScheduler):
         **kwargs,
     ) -> None:
         super().__init__(**kwargs)
-        self.folder_path = str(self.folder_path)
         if glob_pattern is not None:
-            self.folder_path = os.path.join(self.folder_path, glob_pattern)
-        self.path_in_repo = str(self.path_in_repo)
-        self.logger = logger
+            self.folder_path = self.folder_path / glob_pattern
         self.delete_after_upload = delete_after_upload
         self.overwrite = overwrite
         self.zip = zip
         self.reverse = reverse
         self.is_done = False
-        self.logger.info(f"Created UploadScheduler for {self.repo_id} repo")
+        logging.info(f"Created UploadScheduler for {self.repo_id} repo")
 
     def push_to_hub(self):
         """
         Push files to HuggingFace repository.
         """
-        self.logger.info("Start new pushing cycle")
+        logging.info("Start new pushing cycle")
         paths = sorted(glob(self.folder_path), reverse=self.reverse)
-        self.logger.info(f"Found {len(paths)} to be pushed")
+        logging.info(f"Found {len(paths)} to be pushed")
         if len(paths) == 0:
             self.is_done = True
             return
 
         for i, path in enumerate(paths):
-            dest_path = os.path.join(
-                self.path_in_repo, os.path.basename(path)
-            )
-            if os.path.isdir(path) and self.zip:
+            path = Path(path)
+            dest_path = self.path_in_repo / path.relative_to(self.folder_path)
+            if path.is_dir() and self.zip:
                 dest_path += ".zip"
                 zipping = True
             else:
@@ -63,15 +60,15 @@ class UploadScheduler(CommitScheduler):
                 repo_type=self.repo_type,
             ):
                 continue
-            self.logger.info(f"[{i + 1}/{len(paths)}] Uploading {path}")
+            logging.info(f"[{i + 1}/{len(paths)}] Uploading {path}")
 
             if zipping:
                 with tempfile.TemporaryDirectory() as temp_dir:
-                    src_path = os.path.join(temp_dir, os.path.basename(path))
+                    temp_dir = Path(temp_dir)
+                    src_path = temp_dir / path.name
                     zip_dir(
                         dir_path=path,
                         output_dir=temp_dir,
-                        logger=self.logger,
                     )
                     src_path = src_path + ".zip"
                     upload_to_hf(
@@ -79,10 +76,9 @@ class UploadScheduler(CommitScheduler):
                         dest_path=dest_path,
                         repo_id=self.repo_id,
                         repo_type=self.repo_type,
-                        logger=self.logger,
                     )
                     os.remove(src_path)
-                    self.logger.info(f"Deleted {src_path}")
+                    logging.info(f"Deleted {src_path}")
             else:
                 src_path = path
                 upload_to_hf(
@@ -90,31 +86,29 @@ class UploadScheduler(CommitScheduler):
                     dest_path=dest_path,
                     repo_id=self.repo_id,
                     repo_type=self.repo_type,
-                    logger=self.logger,
                 )
 
             if self.delete_after_upload:
                 os.remove(path)
-                self.logger.info(f"Deleted {path}")
+                logging.info(f"Deleted {path}")
 
         self.is_done = True
 
 
 def upload_to_hf(
-    src_path: str,
-    dest_path: str,
+    src_path: Union[str, Path],
+    dest_path: Union[str, Path],
     repo_id: str,
     repo_type: str,
-    logger: Logger,
 ) -> None:
     """
     Upload a file or directory to HuggingFace repository
 
     Parameters
     ----------
-    src_path : str
+    src_path : Union[str, Path]
         Path to the file or directory to upload
-    dest_path : str
+    dest_path : Union[str, Path]
         Path to upload in the repository
     repo_id : str
         HuggingFace repository ID
@@ -122,7 +116,7 @@ def upload_to_hf(
         Type of the repository
     """
     api = HfApi()
-    if os.path.isdir(src_path):
+    if Path(src_path).is_dir():
         api.upload_folder(
             repo_id=repo_id,
             folder_path=src_path,
@@ -136,48 +130,74 @@ def upload_to_hf(
             repo_id=repo_id,
             repo_type=repo_type,
         )
-    logger.info(f"Uploaded {src_path} to {repo_id} repository")
+    logging.info(f"Uploaded {src_path} to {repo_id} repository")
 
 
-def exists_on_hf(
+def get_hf_path(
     repo_id: str,
-    path_in_repo: str,
+    path_in_repo: Union[str, Path],
     repo_type: str,
-) -> bool:
+) -> Path:
     """
-    Check if a file or directory exists in HuggingFace repository
+    Get the path in the HuggingFace repository.
 
     Parameters
     ----------
     repo_id : str
         HuggingFace repository ID
-    path_in_repo : str
+    path_in_repo : Union[str, Path]
         Path to check in the repository
     repo_type : str
         Type of the repository
 
     Returns
     -------
-    bool
-        True if the file or directory exists
+    Path
+        Path in the repository.
     """
     if repo_type == "dataset":
-        hf_path = f"datasets/{repo_id}/{path_in_repo}"
+        hf_path = Path(f"datasets/{repo_id}")
     elif repo_type == "model":
-        hf_path = f"{repo_id}/{path_in_repo}"
+        hf_path = Path(repo_id)
     else:
         raise ValueError("repo_type should be either 'dataset' or 'model'")
+    return hf_path / path_in_repo
+
+
+def exists_on_hf(
+    repo_id: str,
+    path_in_repo: Union[str, Path],
+    repo_type: str,
+) -> bool:
+    """
+    Check if a file or directory exists in HuggingFace repository.
+
+    Parameters
+    ----------
+    repo_id : str
+        HuggingFace repository ID.
+    path_in_repo : Union[str, Path]
+        Path to check in the repository.
+    repo_type : str
+        Type of the repository.
+
+    Returns
+    -------
+    bool
+        True if the file or directory exists.
+    """
+    hf_path = get_hf_path(repo_id, path_in_repo, repo_type)
     fs = HfFileSystem()
     return fs.exists(hf_path)
 
 
-def get_paths(path: str) -> list:
+def get_paths(path: Union[Path, str]) -> list:
     """
     Get all paths in the HuggingFace repository.
 
     Parameters
     ----------
-    path : str
+    path : Union[Path, str]
         Path to the repository.
 
     Returns
@@ -197,12 +217,11 @@ def get_paths(path: str) -> list:
 
 def download_from_hf(
     repo_id: str,
-    path_in_repo: str,
-    output_dir: str,
-    logger: Logger,
+    path_in_repo: Union[str, Path],
+    output_dir: Union[str, Path],
     overwrite: bool = False,
     repo_type: str = "dataset",
-):
+) -> None:
     """
     Download files from HuggingFace repository.
 
@@ -210,53 +229,39 @@ def download_from_hf(
     ----------
     repo_id : str
         HuggingFace repository ID.
-    path_in_repo : str
+    path_in_repo : Union[str, Path]
         Path to download in the repository.
-    output_dir : str
+    output_dir : Union[str, Path]
         Output directory.
     overwrite : bool, False
         Overwrite existing files.
     repo_type : str, 'dataset'
         Type of the repository.
     """
-    if repo_type == "dataset":
-        hf_root_path = f"datasets/{repo_id}/{path_in_repo}"
-    elif repo_type == "model":
-        hf_root_path = f"{repo_id}/{path_in_repo}"
-    else:
-        logger.error("repo_type should be either 'dataset' or 'model'")
-        return
-
     fs = HfFileSystem()
-    os.makedirs(output_dir, exist_ok=True)
+    output_dir = Path(output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    hf_root_path = get_hf_path(repo_id, path_in_repo, repo_type)
     hf_paths = get_paths(hf_root_path)
-    hf_root_path = os.path.normpath(hf_root_path)
+
     for i, (root, file) in enumerate(hf_paths):
+        root = Path(root)
         if file is None:
             file_output_dir = output_dir
-            file = os.path.basename(root)
-            root = os.path.dirname(root)
+            file, root = root.name, root.parent
         else:
-            file_output_dir = os.path.join(
-                output_dir,
-                os.path.relpath(root, os.path.dirname(hf_root_path)),
-            )
-        logger.info(f"[{i + 1}/{len(hf_paths)}] Processing {root}/{file}")
+            file_output_dir = output_dir / root.relative_to(hf_root_path.name)
+        logging.info(f"[{i + 1}/{len(hf_paths)}] Processing {root}/{file}")
 
-        if not os.path.exists(file_output_dir):
-            os.makedirs(file_output_dir, exist_ok=True)
+        file_output_dir.mkdir(parents=True, exist_ok=True)
 
-        if overwrite or not os.path.exists(os.path.join(file_output_dir, file)):
+        if overwrite or not (file_output_dir / file).exists():
             try:
-                fs.download(
-                    f"{root}/{file}",
-                    file_output_dir,
-                    verbose=False,
-                )
-                logger.info(f"\tDownloaded to {file_output_dir}")
+                fs.download(root / file, file_output_dir, verbose=False)
+                logging.info(f"\tDownloaded to {file_output_dir}")
             except KeyboardInterrupt:
-                logger.info("\tInterrupted by user")
-                os.remove(os.path.join(file_output_dir, file))
+                logging.info("\tInterrupted by user")
+                os.remove(file_output_dir / file)
                 exit()
         else:
-            logger.info(f"\tFile exists in {file_output_dir}")
+            logging.info(f"\tFile exists in {file_output_dir}")
