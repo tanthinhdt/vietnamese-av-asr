3b1374e79da401ed0cd6f643f74de6a5cff9020d
tanthinhdt <tanthinhdt24@gmail.com>
2024-07-28 07:41:01 +0700

    chore(src): clean code

--- src/tools/models.py
+++ src/tools/models.py
@@ -1,8 +1,5 @@
 import torch
 import logging
-from time import time
-from typing import Union
-import onnxruntime as ort
 from transformers import (
     FeatureExtractionMixin,
     AutoModel,
@@ -12,11 +9,12 @@ from transformers import (
 from transformers.pipelines import PIPELINE_REGISTRY
 from configs import ModelConfig, InferenceConfig
 from models import (
-    AVHubertConfig, AVHubertFeatureExtractor, AVHubertModel,
-    AVSPLLMConfig, AVSPLLMFeatureExtractor, AVSPLLMModel,
+    AVSPLLMConfig,
+    AVSPLLMFeatureExtractor,
+    AVSPLLMModel,
 )
 from pipelines import AutomaticSpeechRecognitionPipeline
-from utils import MODELS, draw_text_on_image
+from utils import draw_text_on_image
 
 
 def load_model(
@@ -26,17 +24,9 @@ def load_model(
     """
     """
     if do_train:
-        if model_config.arch == "av_hubert":
-            config_class = AVHubertConfig
-            processor_class = AVHubertFeatureExtractor
-            model_class = AVHubertModel
-        elif model_config.arch == "avsp_llm":
-            config_class = AVSPLLMConfig
-            processor_class = AVSPLLMFeatureExtractor
-            model_class = AVSPLLMModel
-        else:
-            logging.error(f"Model {model_config.arch} is not supported")
-            exit(1)
+        config_class = AVSPLLMConfig
+        processor_class = AVSPLLMFeatureExtractor
+        model_class = AVSPLLMModel
 
         config_class.register_for_auto_class()
         processor_class.register_for_auto_class("AutoFeatureExtractor")
@@ -47,7 +37,7 @@ def load_model(
         processor = processor_class(config=config)
         model = model_class(config=config)
 
-        return config, processor, model
+        return processor, model
 
     processor = FeatureExtractionMixin.from_pretrained(
         model_config.pretrained,
@@ -61,7 +51,7 @@ def load_model(
     )
     model.eval()
 
-    return model.config, processor, model
+    return processor, model
 
 
 class Predictions:
@@ -137,64 +127,12 @@ class Predictions:
         return results
 
 
-def get_predictions(
-    inputs: torch.Tensor,
-    model: Union[ort.InferenceSession, AutoModel],
-    id2gloss: dict,
-    k: int = 3,
-) -> Predictions:
-    """
-    Get the top-k predictions.
-    Parameters
-    ----------
-    inputs : torch.Tensor
-        Model inputs (Time, Height, Width, Channels).
-    model : Union[ort.InferenceSession, AutoModel]
-        Model to get predictions from.
-    id2gloss : dict
-        Mapping of class indices to glosses.
-    k : int, optional
-        Number of predictions to return, by default 3.
-    Returns
-    -------
-    tuple
-        List of top-k predictions and inference time.
-    """
-    if inputs is None:
-        return Predictions()
-
-    # Get logits
-    start_time = time()
-    if isinstance(model, ort.InferenceSession):
-        inputs = inputs.cpu().numpy()
-        logits = torch.from_numpy(model.run(None, {"pixel_values": inputs})[0])
-    else:
-        logits = model(inputs.to(model.device)).logits
-    inference_time = time() - start_time
-
-    # Get top-3 predictions
-    topk_scores, topk_indices = torch.topk(logits, k, dim=1)
-    topk_scores = torch.nn.functional.softmax(topk_scores, dim=1).squeeze().detach().numpy()
-    topk_indices = topk_indices.squeeze().detach().numpy()
-    predictions = [
-        {
-            'gloss': id2gloss[str(topk_indices[i])],
-            'score': topk_scores[i],
-        }
-        for i in range(k)
-    ]
-
-    return Predictions(predictions=predictions, inference_time=inference_time)
-
-
 def load_pipeline(
     model_config: ModelConfig,
     inference_config: InferenceConfig = None,
 ) -> Pipeline:
     """
     """
-    assert model_config.arch in MODELS, f"Model {model_config.arch} is not supported"
-
     if inference_config is None:
         _, processor, model = load_model(model_config)
         PIPELINE_REGISTRY.register_pipeline(
@@ -220,45 +158,3 @@ def load_pipeline(
         trust_remote_code=True,
         use_onnx=inference_config.use_onnx,
     )
-
-
-def get_dummy_input(
-    arch: str,
-    processor: Union[FeatureExtractionMixin],
-    batch_size: int = 1,
-) -> tuple:
-    """
-    Get the input shape for the model.
-
-    Parameters
-    ----------
-    processor : Union[FeatureExtractionMixin]
-        Model processor.
-    batch_size : int, optional
-        Batch size, by default 1.
-
-    Returns
-    -------
-    tuple
-        Input shape.
-    """
-    assert arch in MODELS, f"Model {arch} is not supported"
-
-    if arch == "avsp_llm":
-        source = {
-            "audio": torch.randn(
-                batch_size,
-                processor.audio_feat_dim,
-                processor.num_frames,
-            ),
-            "video": torch.randn(
-                batch_size,
-                processor.num_channels,
-                processor.num_frames,
-                processor.height,
-                processor.width,
-            ),
-            "text": torch.randint(1, 1000, (batch_size, processor.text_feat_dim)),
-        }
-        padding_mask = torch.zeros(batch_size, processor.num_frames)
-        return source, padding_mask
--- src/utils/constants.py
+++
@@ -1,3 +0,0 @@
-MODELS = [
-    "avsp_llm",
-]
