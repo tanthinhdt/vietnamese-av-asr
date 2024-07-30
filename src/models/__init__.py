0a11c2a8befc961865ea4e7428bfd910ec22d362
tanthinhdt <tanthinhdt24@gmail.com>
2024-07-30 11:19:42 +0700

    refactor(src): reorganize modules

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,1 @@
-from .av_hubert import AVHubertConfig, AVHubertFeatureExtractor, AVHubertModel
-from .avsp_llm import AVSPLLMConfig, AVSPLLMFeatureExtractor, AVSPLLMModel
 from .sequence_generator import SequenceGenerator
-
