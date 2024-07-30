4ad4f56cf94e4b542fbd08b67283c129a678dab1
tanthinhdt <tanthinhdt24@gmail.com>
2024-07-30 10:13:58 +0700

    feat(src): add process data

---
+++ src/process_data.py
@@ -0,0 +1,59 @@
+from argparse import Namespace
+from dataclasses import dataclass
+from simple_parsing import ArgumentParser, subgroups
+from data import (
+    create_manifest,
+    dump_feature,
+    learn_kmeans,
+    dump_label,
+    count_clusters,
+)
+from configs import (
+    ProcessConfig,
+    CreateManifestConfig,
+    DumpFeatureConfig,
+    LearnKmeansConfig,
+    DumpLabelConfig,
+    CountClustersConfig,
+)
+
+
+@dataclass
+class Config:
+    process: ProcessConfig = subgroups(
+        {
+            "create_manifest": CreateManifestConfig,
+            "dump_feature": DumpFeatureConfig,
+            "learn_kmeans": LearnKmeansConfig,
+            "dump_label": DumpLabelConfig,
+            "count_clusters": CountClustersConfig,
+        },
+        default="create_manifest",
+    )
+
+
+def get_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_arguments(Config, dest="config")
+    args = parser.parse_args()
+    return args
+
+
+def main(args: Namespace) -> None:
+    config = args.config.process
+
+    if isinstance(config, CreateManifestConfig):
+        create_manifest(config)
+    elif isinstance(config, DumpFeatureConfig):
+        dump_feature(config)
+    elif isinstance(config, LearnKmeansConfig):
+        learn_kmeans(config)
+    elif isinstance(config, DumpLabelConfig):
+        dump_label(config)
+    elif isinstance(config, CountClustersConfig):
+        count_clusters(config)
+
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args=args)
