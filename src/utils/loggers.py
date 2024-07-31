16471c6c1e40a3d5b0869fd7a5d78a42cde6d4b7
tanthinhdt <tanthinhdt24@gmail.com>
2024-07-31 11:54:53 +0700

    fix(src/utils/loggers): fix config logger

--- src/utils/loggers.py
+++ src/utils/loggers.py
@@ -6,5 +6,5 @@ from typing import Union
 def config_logger(log_file: Union[Path, str] = None) -> None:
     if log_file is not None:
         log_file = Path(log_file)
-        log_file.mkdir(parents=True, exist_ok=True)
+        log_file.parent.mkdir(parents=True, exist_ok=True)
         logger.add(log_file)
