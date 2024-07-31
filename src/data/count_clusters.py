0258f62c777c773268b08ea14368db6097218871
tanthinhdt <tanthinhdt24@gmail.com>
2024-07-31 07:11:56 +0700

    update(src/data/count_clusters): update process

--- src/data/count_clusters.py
+++ src/data/count_clusters.py
@@ -9,12 +9,18 @@ def count_clusters(config: CountClustersConfig) -> None:
     for rank in range(config.nshard):
         label_file = config.lab_dir / f"{config.split}_{rank}_{config.nshard}.km"
         with open(label_file) as f:
-            samples.extend(f.readlines())
-            num_frames_before += len(f.read().split())
+            for line in f.readlines():
+                samples.append(line)
+                num_frames_before += len(line.split())
     logger.info(f"Loaded {len(samples)} samples")
     logger.info(f"Total number of frames: {num_frames_before}")
 
-    counts = []
+    label_file = config.output_dir / f"{config.split}.km"
+    with open(label_file, 'w') as f:
+        f.write('\n'.join(samples))
+    logger.info(f"Saved labels to {label_file}")
+
+    counts_list = []
     num_frames_after = 0
     for unit_line in tqdm(samples):
         unit_line = unit_line.strip().split(' ')
@@ -29,10 +35,14 @@ def count_clusters(config: CountClustersConfig) -> None:
                 current_count = 1
         counts.append(current_count)
         str_counts = [str(x) for x in counts]
-        counts.append(' '.join(str_counts) + '\n')
-        num_frames_after += len(unit_line)
+        counts_list.append(' '.join(str_counts) + '\n')
+        num_frames_after += len(counts)
     logger.info(f"Total number of frames after dedup will be: {num_frames_after}")
+    logger.info(f"Total number of frames removed: {num_frames_before - num_frames_after}")
 
     cluster_counts_file = config.output_dir / f"{config.split}.cluster_counts"
     with open(cluster_counts_file, 'w') as f:
-        f.write(''.join(counts))
+        f.write(''.join(counts_list))
+    logger.info(f"Saved cluster counts to {cluster_counts_file}")
+
+    logger.info("Done!")
