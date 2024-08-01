340678c0cbfb667b870a2f957bca076879fb6ede
tanthinhdt <tanthinhdt24@gmail.com>
2024-08-01 09:22:36 +0700

    chore(src/modules): update init

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -1,2 +1,7 @@
 from .av_hubert import AVHubertConfig, AVHubertFeatureExtractor, AVHubertModel
-from .avsp_llm import AVSPLLMConfig, AVSPLLMFeatureExtractor, AVSPLLMModel
+from .avsp_llm import (
+    AVSPLLMConfig,
+    GenerationConfig,
+    AVSPLLMFeatureExtractor,
+    AVSPLLMModel,
+)
