23eca479c22093deec1aef238d671d1da29c6880
tanthinhdt <tanthinhdt24@gmail.com>
2024-08-03 09:42:04 +0700

    fix(src/visualization/save_frames): fix save frames

--- src/visualization/save_frames.py
+++ src/visualization/save_frames.py
@@ -13,9 +13,10 @@ def save_frames(config: SaveFramesConfig):
 
     for video_file in tqdm(video_files):
         logger.info(f"Processing {video_file}")
-        output_dir = config.output_dir / video_file.name
+        output_dir = config.output_dir / video_file.with_suffix("").name
 
         if not config.overwrite and output_dir.exists():
+            logger.info(f"Skipping {video_file} as it already exists")
             continue
 
         video = cv2.VideoCapture(str(video_file))
@@ -32,5 +33,7 @@ def save_frames(config: SaveFramesConfig):
             frame_path = output_dir / f"{frame_count}.jpg"
             cv2.imwrite(frame_path, frame)
 
+        logger.info(f"Saved {frame_count} frames to {output_dir}")
         video.release()
+
     logger.info("Done!")
