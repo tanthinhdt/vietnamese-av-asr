9f9cabace95a0aa4991a1d33bd78d8d100d4d20e
tanthinhdt <tanthinhdt24@gmail.com>
2024-08-14 13:24:23 +0700

    feat(scripts): add script for decode

--- scripts/decode.sh
+++ scripts/decode.sh
@@ -1,48 +1,23 @@
-#! /bin/bash
-# Copyright (c) Meta Platforms, Inc. and its affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
-LANG=en    # language direction (e.g 'en' for VSR task / 'en-es' for En to Es VST task)
-
-# set paths
 ROOT=$(dirname "$(dirname "$(readlink -fm "$0")")")
-MODEL_SRC=${ROOT}/src
-LLM_PATH=${ROOT}/checkpoints/Llama-2-7b-hf   # path to llama checkpoint
-DATA_ROOT=${MODEL_SRC}/dataset   # path to test dataset dir
+EXP=ViAVSP-LLM_v2.0
+EVAL_SET=test
 
-MODEL_PATH=${ROOT}/checkpoints/checkpoint_finetune.pt  # path to trained model
-OUT_PATH=${ROOT}/decode    # output path to save
+DATA_PATH=${ROOT}/data/processed/vasr/audio-visual/full
+MODEL_PATH=${ROOT}/models/${EXP}/checkpoints/checkpoint_best.pt
+OUT_PATH=${ROOT}/evaluations/${EXP}/${EVAL_SET}
 
-# fix variables based on langauge
-if [[ $LANG == *"-"* ]] ; then
-    TASK="vst"
-    IFS='-' read -r SRC TGT <<< ${LANG}
-    USE_BLEU=true
-    DATA_PATH=${DATA_ROOT}/${TASK}/${SRC}/${TGT}
+USE_BLEU=false
+SRC_DIR=${ROOT}/src
+LLM_PATH=vilm/vinallama-2.7b 
 
-else
-    TASK="vsr"
-    TGT=${LANG}
-    USE_BLEU=false
-    DATA_PATH=${DATA_ROOT}/${TASK}/${LANG}
-fi
-
-# start decoding
-export PYTHONPATH="${ROOT}/fairseq:$PYTHONPATH"
-CUDA_VISIBLE_DEVICES=0 python -B ${MODEL_SRC}/vsp_llm_decode.py \
-    --config-dir ${MODEL_SRC}/conf \
-    --config-name s2s_decode \
-        common.user_dir=${MODEL_SRC} \
-        dataset.gen_subset=test \
+CUDA_VISIBLE_DEVICES=0 python -B ${SRC_DIR}/decode.py \
+    --config-dir ${SRC_DIR}/configs/inference \
+    --config-name decode \
+        common.user_dir=${SRC_DIR} \
+        dataset.gen_subset=${EVAL_SET} \
         override.data=${DATA_PATH} \
         override.label_dir=${DATA_PATH} \
-        generation.beam=20 \
-        generation.lenpen=0 \
-        dataset.max_tokens=3000 \
         override.eval_bleu=${USE_BLEU} \
         override.llm_ckpt_path=${LLM_PATH} \
         common_eval.path=${MODEL_PATH} \
-        common_eval.results_path=${OUT_PATH}/${TASK}/${LANG}
+        common_eval.results_path=${OUT_PATH} \
