da123a94bcee510cd16894294608aa0d1396ff3d
tanthinhdt <tanthinhdt24@gmail.com>
2024-08-14 13:43:03 +0700

    chore(scripts): clean code

--- scripts/decode.sh
+++ scripts/decode.sh
@@ -2,21 +2,22 @@ ROOT=$(dirname "$(dirname "$(readlink -fm "$0")")")
 EXP=ViAVSP-LLM_v2.0
 EVAL_SET=test
 
-DATA_PATH=${ROOT}/data/processed/vasr/audio-visual/full
-MODEL_PATH=${ROOT}/models/${EXP}/checkpoints/checkpoint_best.pt
-OUT_PATH=${ROOT}/evaluations/${EXP}/${EVAL_SET}
+DATA_DIR=${ROOT}/data/processed/vasr/audio-visual/full
+EXP_DIR=${ROOT}/models
+MODEL_PATH=${EXP_DIR}/${EXP}/checkpoints/checkpoint_best.pt
+LLM_PATH=vilm/vinallama-2.7b 
 
 USE_BLEU=false
+OUT_PATH=${EXP_DIR}/evaluations/${EVAL_SET}
 SRC_DIR=${ROOT}/src
-LLM_PATH=vilm/vinallama-2.7b 
 
 CUDA_VISIBLE_DEVICES=0 python -B ${SRC_DIR}/decode.py \
     --config-dir ${SRC_DIR}/configs/inference \
     --config-name decode \
         common.user_dir=${SRC_DIR} \
         dataset.gen_subset=${EVAL_SET} \
-        override.data=${DATA_PATH} \
-        override.label_dir=${DATA_PATH} \
+        override.data=${DATA_DIR} \
+        override.label_dir=${DATA_DIR} \
         override.eval_bleu=${USE_BLEU} \
         override.llm_ckpt_path=${LLM_PATH} \
         common_eval.path=${MODEL_PATH} \
--- scripts/train.sh
+++ scripts/train.sh
@@ -3,11 +3,11 @@ EXP=ViAVSP-LLM_v2.0
 
 DATA_DIR=${ROOT}/data/processed/vasr/audio-visual/full
 EXP_DIR=${ROOT}/models
-OUT_PATH=${EXP_DIR}/${EXP}
-
-SRC_DIR=${ROOT}/src
-LLM_PATH=vilm/vinallama-2.7b
 PRETRAINED_MODEL_PATH=${ROOT}/models/AV-Hubert/large_vox_iter5.pt
+LLM_PATH=vilm/vinallama-2.7b
+
+OUT_DIR=${EXP_DIR}/${EXP}
+SRC_DIR=${ROOT}/src
 
 fairseq-hydra-train \
     --config-dir ${SRC_DIR}/configs/training \
@@ -18,4 +18,4 @@ fairseq-hydra-train \
         task.llm_ckpt_path=${LLM_PATH} \
         model.w2v_path=${PRETRAINED_MODEL_PATH} \
         model.llm_ckpt_path=${LLM_PATH} \
-        hydra.run.dir=${OUT_PATH} \
+        hydra.run.dir=${OUT_DIR} \
